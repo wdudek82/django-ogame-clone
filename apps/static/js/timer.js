381e5f6b3b08f8fd233299cab7ee1c9cf9c82806
Wojciech Dudek <wdudek82@gmail.com>
2017-11-25 11:48:37 +0100

    Building timer doesn't show negative timestamps now

--- apps/static/js/timer.js
+++ apps/static/js/timer.js
@@ -1,34 +1,35 @@
 // var this_js_script = $('script[src*=timer]');
 var selector = $('#timer');
-var total_seconds = parseFloat(selector.attr('data-datetime'));
-var datetime = new Date(total_seconds);
+var total_milliseconds = parseFloat(selector.attr('data-datetime'));
+var datetime = new Date(total_milliseconds);
 
+if (datetime > Date.now()) {
+    setInterval(function showTimer() {
+        var delta = Math.floor((datetime - Date.now()));
+        console.log(delta);
+        console.log(datetime);
+        console.log(datetime > Date.now());
 
-setInterval(function showTimer() {
-    var delta = Math.floor((datetime - Date.now()));
-    console.log(delta);
-    console.log(datetime);
-    console.log(datetime > Date.now());
+        var timer = $('#timer');
+        if (delta <= 600) {
+            timer.css('color', 'red');
+        } else {
+            timer.css('color', 'black');
+        }
 
-    var timer = $('#timer');
-    if (delta <= 600) {
-        timer.css('color', 'red');
-    } else {
-        timer.css('color', 'black');
-    }
+        var timeRemaining = new Date(delta);
+        var day = 24 * 60 * 60 * 1000;  // hours * minutes * seconds * milliseconds
+        var hour = 60 * 60 * 1000;
+        var rDays = Math.floor(delta / day) ? Math.floor(delta / day) + 'd ' : '';
+        var rHours = Math.floor(delta % day / hour) ? Math.floor(delta % day / hour) + 'h ' : '';
+        // var rHours = (timeRemaining.getHours() < 10 ? '0' : '') + timeRemaining.getHours();
+        var rMinutes = (timeRemaining.getMinutes() < 10 ? '0' : '') + timeRemaining.getMinutes();
+        var rSeconds = (timeRemaining.getSeconds() < 10 ? '0' : '') + timeRemaining.getSeconds();
+        var formatedRemainingTime = rDays + rHours + rMinutes + 'm ' + rSeconds + 's';
+        console.log(formatedRemainingTime);
+        console.log('\n\n');
 
-    var timeRemaining = new Date(delta);
-    var day = 24 * 60 * 60 * 1000;  // hours * minutes * seconds * milliseconds
-    var hour = 60 * 60 * 1000;
-    var rDays = Math.floor(delta / day) ? Math.floor(delta / day) + 'd ' : '';
-    var rHours = Math.floor(delta % day / hour) ? Math.floor(delta % day / hour) + 'h ' : '';
-    // var rHours = (timeRemaining.getHours() < 10 ? '0' : '') + timeRemaining.getHours();
-    var rMinutes = (timeRemaining.getMinutes() < 10 ? '0' : '') + timeRemaining.getMinutes();
-    var rSeconds = (timeRemaining.getSeconds() < 10 ? '0' : '') + timeRemaining.getSeconds();
-    var formatedRemainingTime = rDays + rHours + rMinutes + 'm ' + rSeconds + 's';
-    console.log(formatedRemainingTime);
-    console.log('\n\n');
+        timer.text(formatedRemainingTime);
 
-    timer.text(formatedRemainingTime);
-
-}, 1000);
+    }, 1000);
+}
